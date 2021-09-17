acdbb51b6ca76b3176d0f912f0210f32dfbb185c
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2021-09-17 10:46:44 -0300

    Add "name" to animeList

--- lesson5.js
+++ lesson5.js
@@ -1,11 +1,21 @@
 let animeList = [
-    "https://i.imgur.com/9HSI4fT.jpeg",
-    "https://animefire.net/img/animes/odd-taxi-large.webp?v=1",
-    "https://pm1.narvii.com/6251/4227229f395b72d03953a2fd56e05400895bdb7a_hq.jpg"
+    {
+        name: "Naruto",
+        imgLink: "https://i.imgur.com/9HSI4fT.jpeg"
+    },
+    {
+        name: "Odd Taxi",
+        imgLink: "https://animefire.net/img/animes/odd-taxi-large.webp?v=1"
+    },
+    {
+        name: "Aria",
+        imgLink: "https://pm1.narvii.com/6251/4227229f395b72d03953a2fd56e05400895bdb7a_hq.jpg"
+    }
 ];
   
 for(let i = 0; i < animeList.length; i++)
 {
-    document.write("<img class=\"anime\" src=" + animeList[i] + ">");
+    document.write("<img class=\"anime\" src=" + animeList[i].imgLink + ">");
 }
+
   
